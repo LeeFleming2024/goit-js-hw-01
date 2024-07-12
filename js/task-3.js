6a36256d85b0d798db6fbd0b3d70055cd7911325
Lee Fleming <fl9379992@gmail.com>
2024-07-12 22:12:28 +0300

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,9 @@
 function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(border) * 2;
-    const totalWidth = contentWidth + paddingWidth + borderWidth;
-    return totalWidth;
+    const paddingtWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const elementWidth = contentWidth + paddingtWidth * 2 + borderWidth * 2;
+    return elementWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
